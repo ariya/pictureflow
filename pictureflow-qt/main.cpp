22a3be44d06b0baae152930f2b89fdc3df151d09
Ariya Hidayat <ariya.hidayat@gmail.com>
2010-05-16 02:19:19 -0700

    Switch to use the coding style similar to Qt.

--- pictureflow-qt/main.cpp
+++ pictureflow-qt/main.cpp
@@ -31,123 +31,116 @@
 
 QStringList findFiles(const QString& path = QString())
 {
-  QStringList files;
+    QStringList files;
 
-  QDir dir = QDir::current();
-  if(!path.isEmpty())
-    dir = QDir(path);
+    QDir dir = QDir::current();
+    if (!path.isEmpty())
+        dir = QDir(path);
 
-  dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
-  QFileInfoList list = dir.entryInfoList();
-  for (int i = 0; i < list.size(); ++i) 
-    files += dir.absoluteFilePath(list.at(i).fileName());
+    dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
+    QFileInfoList list = dir.entryInfoList();
+    for (int i = 0; i < list.size(); ++i)
+        files += dir.absoluteFilePath(list.at(i).fileName());
 
-  return files;
+    return files;
 }
 
 class Browser: public PictureFlow
 {
 public:
-  Browser(): PictureFlow()
-  {
-    setWindowTitle("PictureFlow");
-  }
-
-  void keyPressEvent(QKeyEvent* event)
-  {
-    if(event->key() == Qt::Key_Escape || event->key() == Qt::Key_Enter || 
-      event->key() == Qt::Key_Return)
-      {
-        event->accept();
-        close();
-      }
-
-    // checking the speed of rendering
-    if(event->key() == Qt::Key_F10)
-    if(event->modifiers() == Qt::AltModifier)
-    {
-      qDebug("benchmarking.... please wait");
-      const int blit_count = 10;
-
-      QTime stopwatch;
-      stopwatch.start();
-      for(int i = 0; i < blit_count; i++)
-      {
-        render(); repaint(); QApplication::flush(); QApplication::syncX();
-        render(); repaint(); QApplication::flush(); QApplication::syncX();
-        render(); repaint(); QApplication::flush(); QApplication::syncX();
-        render(); repaint(); QApplication::flush(); QApplication::syncX();
-        render(); repaint(); QApplication::flush(); QApplication::syncX();
-        render(); repaint(); QApplication::flush(); QApplication::syncX();
-        render(); repaint(); QApplication::flush(); QApplication::syncX();
-        render(); repaint(); QApplication::flush(); QApplication::syncX();
-        render(); repaint(); QApplication::flush(); QApplication::syncX();
-        render(); repaint(); QApplication::flush(); QApplication::syncX();
-      }
-      QString msg;
-      int elapsed = stopwatch.elapsed();
-      if( elapsed > 0 )
-        msg = QString("FPS: %1").arg( blit_count*10*1000.0/elapsed );
-      else
-        msg = QString("Too fast. Increase blit_count");
-      setWindowTitle( msg );
-      event->accept();
-      return;
+    Browser(): PictureFlow() {
+        setWindowTitle("PictureFlow");
     }
 
-    // for debugging only: Alt+F11 cycles the reflection effect
-    if(event->key() == Qt::Key_F11)
-    if(event->modifiers() == Qt::AltModifier)
-    {
-      qDebug("changing reflection effect...");
-      switch(reflectionEffect())
-      {
-        //case NoReflection:      setReflectionEffect(PlainReflection); break;
-        case PlainReflection:   setReflectionEffect(BlurredReflection); break;
-        case BlurredReflection: setReflectionEffect(PlainReflection); break;
-        default:                setReflectionEffect(PlainReflection); break;
-      }
-      event->accept();
-      return;
+    void keyPressEvent(QKeyEvent* event) {
+        if (event->key() == Qt::Key_Escape || event->key() == Qt::Key_Enter ||
+                event->key() == Qt::Key_Return) {
+            event->accept();
+            close();
+        }
+
+        // checking the speed of rendering
+        if (event->key() == Qt::Key_F10)
+            if (event->modifiers() == Qt::AltModifier) {
+                qDebug("benchmarking.... please wait");
+                const int blit_count = 10;
+
+                QTime stopwatch;
+                stopwatch.start();
+                for (int i = 0; i < blit_count; i++) {
+                    render(); repaint(); QApplication::flush(); QApplication::syncX();
+                    render(); repaint(); QApplication::flush(); QApplication::syncX();
+                    render(); repaint(); QApplication::flush(); QApplication::syncX();
+                    render(); repaint(); QApplication::flush(); QApplication::syncX();
+                    render(); repaint(); QApplication::flush(); QApplication::syncX();
+                    render(); repaint(); QApplication::flush(); QApplication::syncX();
+                    render(); repaint(); QApplication::flush(); QApplication::syncX();
+                    render(); repaint(); QApplication::flush(); QApplication::syncX();
+                    render(); repaint(); QApplication::flush(); QApplication::syncX();
+                    render(); repaint(); QApplication::flush(); QApplication::syncX();
+                }
+                QString msg;
+                int elapsed = stopwatch.elapsed();
+                if (elapsed > 0)
+                    msg = QString("FPS: %1").arg(blit_count * 10 * 1000.0 / elapsed);
+                else
+                    msg = QString("Too fast. Increase blit_count");
+                setWindowTitle(msg);
+                event->accept();
+                return;
+            }
+
+        // for debugging only: Alt+F11 cycles the reflection effect
+        if (event->key() == Qt::Key_F11)
+            if (event->modifiers() == Qt::AltModifier) {
+                qDebug("changing reflection effect...");
+                switch (reflectionEffect()) {
+                    //case NoReflection:      setReflectionEffect(PlainReflection); break;
+                case PlainReflection:   setReflectionEffect(BlurredReflection); break;
+                case BlurredReflection: setReflectionEffect(PlainReflection); break;
+                default:                setReflectionEffect(PlainReflection); break;
+                }
+                event->accept();
+                return;
+            }
+
+        PictureFlow::keyPressEvent(event);
     }
+};
 
-    PictureFlow::keyPressEvent(event);  
-  }
-};  
-
-int main( int argc, char ** argv )
+int main(int argc, char ** argv)
 {
-  QApplication* app = new QApplication( argc, argv );
-  Browser* w = new Browser;
+    QApplication* app = new QApplication(argc, argv);
+    Browser* w = new Browser;
 
 #if defined(_WS_QWS) || defined(Q_WS_QWS)
-  w->showFullScreen();
-  int ww = w->width();
-  int wh = w->height();
-  int dim = (ww > wh) ? wh : ww;
-  dim = dim * 3 / 4;
-  w->setSlideSize(QSize(3*dim/5, dim));
-#else  
-  w->setSlideSize(QSize(3*40, 5*40));
-  w->resize(750, 270);
+    w->showFullScreen();
+    int ww = w->width();
+    int wh = w->height();
+    int dim = (ww > wh) ? wh : ww;
+    dim = dim * 3 / 4;
+    w->setSlideSize(QSize(3*dim / 5, dim));
+#else
+    w->setSlideSize(QSize(3*40, 5*40));
+    w->resize(750, 270);
 #endif
 
-  QStringList files = (argc > 1) ? findFiles(QString(argv[1])) : findFiles();
+    QStringList files = (argc > 1) ? findFiles(QString(argv[1])) : findFiles();
 
-  QImage img;
-  for(int i = 0; i < (int)files.count(); i++)
-    if(img.load(files[i]))
-      w->addSlide(img);
+    QImage img;
+    for (int i = 0; i < (int)files.count(); i++)
+        if (img.load(files[i]))
+            w->addSlide(img);
 
-  w->setCenterIndex(w->slideCount()/2);
-  w->setBackgroundColor(Qt::white);
-  w->show();
+    w->setCenterIndex(w->slideCount() / 2);
+    w->setBackgroundColor(Qt::white);
+    w->show();
 
-  app->connect( app, SIGNAL(lastWindowClosed()), app, SLOT(quit()) );
-  int result = app->exec();
+    app->connect(app, SIGNAL(lastWindowClosed()), app, SLOT(quit()));
+    int result = app->exec();
 
-  delete w;
-  delete app;
+    delete w;
+    delete app;
 
-  return result;
+    return result;
 }
--- pictureflow-qt/pictureflow.h
+++ pictureflow-qt/pictureflow.h
@@ -33,170 +33,169 @@
 class PictureFlowPrivate;
 
 /*!
-  Class PictureFlow implements an image show widget with animation effect 
-  like Apple's CoverFlow (in iTunes and iPod). Images are arranged in form 
-  of slides, one main slide is shown at the center with few slides on 
-  the left and right sides of the center slide. When the next or previous 
-  slide is brought to the front, the whole slides flow to the right or 
-  the right with smooth animation effect; until the new slide is finally 
+  Class PictureFlow implements an image show widget with animation effect
+  like Apple's CoverFlow (in iTunes and iPod). Images are arranged in form
+  of slides, one main slide is shown at the center with few slides on
+  the left and right sides of the center slide. When the next or previous
+  slide is brought to the front, the whole slides flow to the right or
+  the right with smooth animation effect; until the new slide is finally
   placed at the center.
 
- */ 
+ */
 class PictureFlow : public QWidget
 {
-Q_OBJECT
+    Q_OBJECT
 
-  Q_PROPERTY(QColor backgroundColor READ backgroundColor WRITE setBackgroundColor)
-  Q_PROPERTY(QSize slideSize READ slideSize WRITE setSlideSize)
-  Q_PROPERTY(int slideCount READ slideCount)
-  Q_PROPERTY(int centerIndex READ centerIndex WRITE setCenterIndex)
+    Q_PROPERTY(QColor backgroundColor READ backgroundColor WRITE setBackgroundColor)
+    Q_PROPERTY(QSize slideSize READ slideSize WRITE setSlideSize)
+    Q_PROPERTY(int slideCount READ slideCount)
+    Q_PROPERTY(int centerIndex READ centerIndex WRITE setCenterIndex)
 
 public:
 
-  enum ReflectionEffect
-  {
-    NoReflection,
-    PlainReflection,
-    BlurredReflection
-  };
+    enum ReflectionEffect {
+        NoReflection,
+        PlainReflection,
+        BlurredReflection
+    };
 
-  /*!
-    Creates a new PictureFlow widget.
-  */  
-  PictureFlow(QWidget* parent = 0);
+    /*!
+      Creates a new PictureFlow widget.
+    */
+    PictureFlow(QWidget* parent = 0);
 
-  /*!
-    Destroys the widget.
-  */
-  ~PictureFlow();
+    /*!
+      Destroys the widget.
+    */
+    ~PictureFlow();
 
-  /*!
-    Returns the background color.
-  */
-  QColor backgroundColor() const;
+    /*!
+      Returns the background color.
+    */
+    QColor backgroundColor() const;
 
-  /*!
-    Sets the background color. By default it is black.
-  */
-  void setBackgroundColor(const QColor& c);
+    /*!
+      Sets the background color. By default it is black.
+    */
+    void setBackgroundColor(const QColor& c);
 
-  /*!
-    Returns the dimension of each slide (in pixels).
-  */  
-  QSize slideSize() const;
+    /*!
+      Returns the dimension of each slide (in pixels).
+    */
+    QSize slideSize() const;
 
-  /*!
-    Sets the dimension of each slide (in pixels).
-  */  
-  void setSlideSize(QSize size);
+    /*!
+      Sets the dimension of each slide (in pixels).
+    */
+    void setSlideSize(QSize size);
 
-  /*!
-    Returns the total number of slides.
-  */
-  int slideCount() const;
+    /*!
+      Returns the total number of slides.
+    */
+    int slideCount() const;
 
-  /*!
-    Returns QImage of specified slide.
-  */  
-  QImage slide(int index) const;
+    /*!
+      Returns QImage of specified slide.
+    */
+    QImage slide(int index) const;
 
-  /*!
-    Returns the index of slide currently shown in the middle of the viewport.
-  */  
-  int centerIndex() const;
+    /*!
+      Returns the index of slide currently shown in the middle of the viewport.
+    */
+    int centerIndex() const;
 
-  /*!
-    Returns the effect applied to the reflection.
-  */  
-  ReflectionEffect reflectionEffect() const;
+    /*!
+      Returns the effect applied to the reflection.
+    */
+    ReflectionEffect reflectionEffect() const;
 
-  /*!
-    Sets the effect applied to the reflection. The default is PlainReflection.
-  */  
-  void setReflectionEffect(ReflectionEffect effect);
+    /*!
+      Sets the effect applied to the reflection. The default is PlainReflection.
+    */
+    void setReflectionEffect(ReflectionEffect effect);
 
 
 public slots:
 
-  /*!
-    Adds a new slide.
-  */  
-  void addSlide(const QImage& image);
+    /*!
+      Adds a new slide.
+    */
+    void addSlide(const QImage& image);
 
-  /*!
-    Adds a new slide.
-  */  
-  void addSlide(const QPixmap& pixmap);
+    /*!
+      Adds a new slide.
+    */
+    void addSlide(const QPixmap& pixmap);
 
-  /*!
-    Removes an existing slide.
-  */
-  void removeSlide(int index);
+    /*!
+      Removes an existing slide.
+    */
+    void removeSlide(int index);
 
-  /*!
-    Sets an image for specified slide. If the slide already exists,
-    it will be replaced.
-  */  
-  void setSlide(int index, const QImage& image);
+    /*!
+      Sets an image for specified slide. If the slide already exists,
+      it will be replaced.
+    */
+    void setSlide(int index, const QImage& image);
 
-  /*!
-    Sets a pixmap for specified slide. If the slide already exists,
-    it will be replaced.
-  */  
-  void setSlide(int index, const QPixmap& pixmap);
+    /*!
+      Sets a pixmap for specified slide. If the slide already exists,
+      it will be replaced.
+    */
+    void setSlide(int index, const QPixmap& pixmap);
 
-  /*!
-    Sets slide to be shown in the middle of the viewport. No animation 
-    effect will be produced, unlike using showSlide.
-  */  
-  void setCenterIndex(int index);
+    /*!
+      Sets slide to be shown in the middle of the viewport. No animation
+      effect will be produced, unlike using showSlide.
+    */
+    void setCenterIndex(int index);
 
-  /*!
-    Clears all slides.
-  */
-  void clear();
+    /*!
+      Clears all slides.
+    */
+    void clear();
 
-  /*!
-    Shows previous slide using animation effect.
-  */
-  void showPrevious();
+    /*!
+      Shows previous slide using animation effect.
+    */
+    void showPrevious();
 
-  /*!
-    Shows next slide using animation effect.
-  */
-  void showNext();
+    /*!
+      Shows next slide using animation effect.
+    */
+    void showNext();
 
-  /*!
-    Go to specified slide using animation effect.
-  */
-  void showSlide(int index);
+    /*!
+      Go to specified slide using animation effect.
+    */
+    void showSlide(int index);
 
-  /*!
-    Rerender the widget. Normally this function will be automatically invoked
-    whenever necessary, e.g. during the transition animation.
-  */
-  void render();
+    /*!
+      Rerender the widget. Normally this function will be automatically invoked
+      whenever necessary, e.g. during the transition animation.
+    */
+    void render();
 
-  /*!
-    Schedules a rendering update. Unlike render(), this function does not cause
-    immediate rendering.
-  */  
-  void triggerRender();
+    /*!
+      Schedules a rendering update. Unlike render(), this function does not cause
+      immediate rendering.
+    */
+    void triggerRender();
 
 signals:
-  void centerIndexChanged(int index);
+    void centerIndexChanged(int index);
 
 protected:
-  void paintEvent(QPaintEvent *event);
-  void keyPressEvent(QKeyEvent* event);
-  void mousePressEvent(QMouseEvent* event);
-  void resizeEvent(QResizeEvent* event);
+    void paintEvent(QPaintEvent *event);
+    void keyPressEvent(QKeyEvent* event);
+    void mousePressEvent(QMouseEvent* event);
+    void resizeEvent(QResizeEvent* event);
 
 private slots:
-  void updateAnimation();
+    void updateAnimation();
 
 private:
-  PictureFlowPrivate* d;
+    PictureFlowPrivate* d;
 };
 
 #endif // PICTUREFLOW_H
